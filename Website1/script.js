5110f950c129ee9cb3db734b71ad7b7cc0807b5b
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-05-17 16:20:55 +0200

    Added a new script which is a javascript and edited html and css files.

---
+++ Website1/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('menu-toggle'); <!--
+const sidebar = document.getElementById('sidebar');
+
+togggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+});
