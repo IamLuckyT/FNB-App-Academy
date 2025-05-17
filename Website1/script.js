7d211f945a4c5cae94d3735e6254cb74e8a06fc3
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-05-17 18:19:34 +0200

    Fixed the errors that was making the sidebar unresponsive

--- Website1/script.js
+++ Website1/script.js
@@ -1,7 +1,7 @@
 const toggleButton = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
 const closeButton = document.getElementById('close-sidebar');
-const overlay =getElementById('overlay');
+const overlay = document.getElementById('overlay');
 
 toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('open');
