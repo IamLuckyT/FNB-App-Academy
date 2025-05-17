ac69fe0ae5bae989443c3adb98f8f82d425ce8e1
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-05-17 17:32:39 +0200

    Added a few features like the toggle button and made it more responsive with css and js

--- Website1/script.js
+++ Website1/script.js
@@ -1,6 +1,26 @@
-const toggleButton = document.getElementById('menu-toggle'); <!--
+const toggleButton = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
+const closeButton = document.getElementById('close-sidebar');
+const overlay =getElementById('overlay');
 
-togggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('open');
+    overlay.classList.toggle('active');
+
+document.body.classList.toggle('no-scroll');
 });
+
+closeButton.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.classList.remove('active');
+
+document.body.classList.remove('no-scroll');
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.classList.remove('active');
+    
+document.body.classList.remove('no-scroll');
+});
+
