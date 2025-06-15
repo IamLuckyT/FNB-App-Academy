2c375ac3eb18259cc494786d20a82b5571c6b33b
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-06-16 00:37:35 +0200

    .

--- Website3/script.js
+++ Website3/script.js
@@ -1,6 +1,12 @@
+//Global variable since its outside the function
+var newLine = true;
+
 //Makes sure that when a user clicks on the button, the button will appear on the screen
 function digitBtnPressed(button){
-  
-  var currentValue = document.getElementById("inputBox").value;
+  if(newLine){
+   document.getElementById("inputBox").value = button;    //If newLine is true, it will replace the current value with the button clicked 
+   newLine = false;                                       //Sets newLine to false so that the next button clicked will not replace the value
+  } else {
+  var currentValue = document.getElementById("inputBox").value;       //Gets the current value in the input box
   document.getElementById("inputBox").value = currentValue + button; //Adds the value clicked and the button
 }
