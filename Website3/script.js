c5023befa5f9afeac6e6da6151ac71c72b6223a6
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-06-15 20:09:26 +0200

    Fixed error of buttons not clicked when the input is focused

--- Website3/script.js
+++ Website3/script.js
@@ -1,6 +1,5 @@
-const digitBtnPressed(button);    // Button that opens the sidebar
 //Makes sure that when a user clicks on the button, the button will appear on the screen
 function digitBtnPressed(button){
   var currentValue = document.getElementById("inputBox").value;
-  document.getElementById(inputBox).value = currentValue + button; //Adds the value clicked and the button
+  document.getElementById("inputBox").value = currentValue + button; //Adds the value clicked and the button
 }
