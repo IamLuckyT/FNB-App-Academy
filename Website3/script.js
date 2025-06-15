a72498a67181f238a60d34c9f49be5e36d38e2f6
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-06-15 20:02:34 +0200

    .

--- Website3/script.js
+++ Website3/script.js
@@ -1,3 +1,4 @@
+const digitBtnPressed(button);    // Button that opens the sidebar
 //Makes sure that when a user clicks on the button, the button will appear on the screen
 function digitBtnPressed(button){
   var currentValue = document.getElementById("inputBox").value;
