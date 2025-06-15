65b0ad6102d7ce93d484361692840dc3fcae9d14
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-06-16 00:06:34 +0200

    .

--- Website3/script.js
+++ Website3/script.js
@@ -1,5 +1,6 @@
 //Makes sure that when a user clicks on the button, the button will appear on the screen
 function digitBtnPressed(button){
+  
   var currentValue = document.getElementById("inputBox").value;
   document.getElementById("inputBox").value = currentValue + button; //Adds the value clicked and the button
 }
