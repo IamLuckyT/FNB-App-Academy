161dedee9af721fb9a47fa371db334d7a4c9c679
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-28 17:51:59 +0200

    Add files via upload

---
+++ Website5/shopping_cart.py
@@ -0,0 +1,12 @@
+foods = []
+prices = []
+total = 8
+
+while True:
+	food = input("Enter food you would like to buy or press q to quit")
+	if food =='q':
+		break
+	else:
+		price = float(input(f'Enter the price of the (food): R")
+		foods.append(food)
+		prices.append(price)
