1af0d6edf41cd81ecd2aa4b0d4f833d8868c1d12
Letlhogonolo Mothoagae <122881376+IamLuckyT@users.noreply.github.com>
2025-08-29 23:29:17 +0000

    Fixed a few typing and syntax errors

--- Website5/shopping_cart.py
+++ Website5/shopping_cart.py
@@ -3,17 +3,17 @@ prices = []
 total = 8
 
 while True:
-	food = input("Enter food you would like to buy or press q to quit")
+	food = input("Enter food you would like to buy or press q to quit: ")
 	if food =='q':
 		break
 	else:
 		price = float(input(f"Enter the price of the (food): R"))
 		foods.append(food)
 		prices.append(price)
-
+		
 print()
 
-print("......... YOUR CART.........")
+print(".................. YOUR CART ..................")
 
 for food in foods:
 	print(food)
